8dd9910ffa72b3cc1cddb2a546c28b12dbf7dcdc
sumanth-botlagunta <sumanthbotlagunta977@gmail.com>
2022-01-09 23:04:36 +0530

    build 3

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ const port = process.env.PORT||9905;
 
 app.use(express.static(path.join(__dirname,'build')));
 
-app.get('/',(req,res) => {
-    res.send('build/index.html')
-});
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static("build"));
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+    });
+  }
+
 
 app.listen(port);
